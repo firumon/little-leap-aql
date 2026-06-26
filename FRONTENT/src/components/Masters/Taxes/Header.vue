73733e446c7f7317ddae7e2345e1392c124eb9f6
Firose Hussain <github@firumon.com>
2026-06-26 12:07:15 +0530

    feat: add custom Taxes header component

---
+++ FRONTENT/src/components/Masters/Taxes/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card>
+    <q-card-section>
+      {{ records }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import {useRecord} from "../../../composables/resources/useRecord.js";
+
+const records = useRecord()
+
+console.log(records)
+</script>
